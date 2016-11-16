8557ef6dc78b7b7c7f5472d00d0748eb1b511718
Roman <kelindar@gmail.com>
2016-11-16 21:23:41 +0100

    cleanup test

--- main.go
+++ main.go
@@ -1,54 +1,45 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"time"
 
 	"./emitter"
 )
 
-var f = func(client emitter.Emitter, msg emitter.Message) {
-	fmt.Printf("TOPIC: %s\n", msg.Topic())
-	//fmt.Printf("MSG: %s\n", msg.Payload())
-
-	var dat map[string]interface{}
-	if err := json.Unmarshal(msg.Payload(), &dat); err != nil {
-		panic(err)
-	}
-
-	fmt.Println(dat["time"])
-}
-
 func main() {
-
+	// Create the options with default values
 	o := emitter.NewClientOptions()
-	o.AddBroker("tcp://api.emitter.io:8080")
-	o.SetClientID("go-client")
-	o.SetKeepAlive(60 * time.Second)
-	o.SetOnMessageHandler(f)
+
+	// Set the message handler
+	o.SetOnMessageHandler(func(client emitter.Emitter, msg emitter.Message) {
+		fmt.Printf("Received message: %s\n", msg.Payload())
+	})
+
+	// Set the presence notification handler
 	o.SetOnPresenceHandler(func(_ emitter.Emitter, p emitter.PresenceEvent) {
 		fmt.Printf("Occupancy: %v\n", p.Occupancy)
 	})
 
+	// Create a new emitter client and connect to the broker
 	c := emitter.NewClient(o)
-
 	sToken := c.Connect()
 	if sToken.Wait() && sToken.Error() != nil {
 		panic("Error on Client.Connect(): " + sToken.Error().Error())
 	}
 
-	//c.Subscribe("z3D7-osAGTU2mQvCvuGXLcMvPXLGGGcy", "cluster")
+	// Subscribe to the presence demo channel
+	c.Subscribe("X4-nUeHjiAygHMdN8wst82S3c2KcCMn7", "presence-demo/1")
 
+	// Publish to the channel
+	c.Publish("X4-nUeHjiAygHMdN8wst82S3c2KcCMn7", "presence-demo/1", "hello")
+
+	// Ask for presence
 	r := emitter.NewPresenceRequest()
 	r.Key = "X4-nUeHjiAygHMdN8wst82S3c2KcCMn7"
 	r.Channel = "presence-demo/1"
 	c.Presence(r)
-	//if x.Wait() && x.Error() != nil {
-	//	panic("Error on Client.Presence(): " + x.Error().Error())
-	//}
 
 	// stop after 10 seconds
 	time.Sleep(10 * time.Second)
-
 }
